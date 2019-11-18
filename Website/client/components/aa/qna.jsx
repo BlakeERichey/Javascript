5d48705037c21e79648686be2aaf07c04c061e1c
Blake <blake.e.richey@gmail.com>
2019-11-18 05:54:01 -0600

    Demo ready

--- Website/client/components/aa/qna.jsx
+++ Website/client/components/aa/qna.jsx
@@ -10,17 +10,34 @@ class Qna extends React.Component{
       <div>
         <label>What are your hobbies?</label>
         <BasicInput onChange={() => console.log()}/>
+        <label>What hobbies do you have?</label>
+        <BasicInput onChange={() => console.log()}/>
         <label>What is your name?</label>
         <BasicInput onChange={() => console.log()}/>
+        <label>What is your first name</label>
+        <BasicInput onChange={() => console.log()}/>
         <label>What skills do you have?</label>
+        <BasicInput onChange={() => console.log()}/>
+        <label>What are your skills?</label>
         <BasicInput onChange={() => console.log()}/>
         <label>How many years have you worked in this industry?</label>
         <BasicInput onChange={() => console.log()}/>
         <label>If you could be any animal, which would you choose?</label>
         <BasicInput onChange={() => console.log()}/>
         
-        <BasicButton onClick={() => {
-          console.log(getAnswer('How many years have you worked in this industry?'));
+        <BasicButton text='Autofill' onClick={() => {
+          let lbls = document.getElementsByTagName('label');
+          console.log(lbls);
+          let nps = document.getElementsByTagName('input');
+          for(let i = 0; i < lbls.length; i++){
+            let question = lbls[i].textContent
+            let answer = getAnswer(question);
+            console.log(answer);
+            if(answer){
+              nps[i].value=answer;
+              nps[i].backgroundColor='#00FF00'
+            }
+          }
         }}/>
       
       </div>
